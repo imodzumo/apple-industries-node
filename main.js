690f9cbf721fde12a9ce50aa64198d373f0afb43
igor <igor.angelchev@tateeda.com>
2024-12-19 03:10:37 +0200

    install

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const data = require('./database/data.json');
+
+app.get('/data', (req, res) => {
+	res.json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Backend server is running on http://localhost:${PORT}`);
+});
